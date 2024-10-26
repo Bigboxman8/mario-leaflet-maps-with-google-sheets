b8165e44481123db81928dadf808856e4044c7c1
Bigboxman8 <bigboxman@hotmail.co.uk>
2024-10-26 20:37:29 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,6 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1y-N9aqxAwrw8cW8K8QqsJ2ZXMTRzn9is/edit?usp=sharing&ouid=106710706586401369069&rtpof=true&sd=true';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1y-N9aqxAwrw8cW8K8QqsJ2ZXMTRzn9is/edit?usp=sharing&ouid=106710706586401369069&rtpof=true&sd=true
+  ';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
